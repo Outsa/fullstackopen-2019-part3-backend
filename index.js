25f9ed51b3c339247acf8b854ee58f27d190aadd
Outi S <outi.syysjoki@gmail.com>
2019-10-25 21:16:38 +0300

    Added morgan logging

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+
+const morgan = require('morgan')
+morgan.token('body', function getBody(req) {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body);
+    }
+    return '';
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
