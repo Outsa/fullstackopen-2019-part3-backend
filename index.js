0072727b0b813e569b35d9fbf0205083ff23c690
Outi S <outi.syysjoki@gmail.com>
2019-11-06 20:59:53 +0200

    Added second version of MongoDB backend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-app.use(express.static('build'))
+require('dotenv').config()
 
 const bodyParser = require('body-parser')
+
+const Person = require('./models/person')
+
 app.use(bodyParser.json())
 
 const morgan = require('morgan')
@@ -41,22 +44,18 @@ let persons = [
     }
 ]
 
-const generateId = () => {
-    let min = Math.ceil(10);
-    let max = Math.floor(100);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+app.use(express.static('build'))
 
 const checkBody = (body) => {
     let errorText = null;
     if (!body.name) {
-        errorText = 'Nimi puuttuu';
+        errorText = 'name missing';
     }
     else if (!body.number) {
-        errorText = 'Numero puuttuu';
+        errorText = 'number missing';
     }
     else if (persons.map(person => person.name).includes(body.name)) {
-        errorText = 'Nimi on jo luettelossa';
+        errorText = 'name must be unique';
     }
     return errorText;
 }
@@ -66,8 +65,11 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons.map(person => person.toJSON()))
+    })
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -79,33 +81,72 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson.toJSON())
+    })
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
     const person = {
         name: body.name,
-        number: body.number,
-        id: generateId(),
+        number: body.number
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
     }
-})
+    next(error)
+}
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
